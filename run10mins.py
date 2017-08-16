fef306be96fd4bdd3ae8e8b335d0eb8506d93105
Ragen Patel <ragenpatel@live.com>
2017-08-16 14:34:26 -0400

    Added safeguards so python script will not execute job if CPU usage is > some % and ram usage is > some %

--- run10mins.py
+++ run10mins.py
@@ -1,12 +1,18 @@
 import schedule
 import time
 import os
+import psutil
 
 def job():
-    print("I'm working...")
-    os.system("javac main.java")
-    os.system("java main")
-    print("I'm finished working")
+    if not psutil.cpu_percent()>10 and not psutil.virtual_memory()[2]>60:
+        print("I'm working...")
+        os.system("javac main.java")
+        os.system("java main")
+        print("I'm finished working")
+    else:
+        print "memory usage:", psutil.virtual_memory()[2], "%"
+        print "cpu usage:", psutil.cpu_percent(), "%"
+        print "*JOB EXECUTION SKIPPED*\n-----------------------\n Reduce memory/CPU usage to continue execution\n-----------------------"
 
 schedule.every(1).seconds.do(job)
 #schedule.every(10).minutes.do(job)
